0e3b37fa9b0ecb4cf55ff0d5a2dcf3500506fc61
Kairo Chácara <kairoepc2@gmail.com>
2025-04-07 18:37:01 -0300

    point

--- Back/app.ts
+++ Back/app.ts
@@ -2,55 +2,15 @@
  * @author Kairo Chácara
  * @version 1.0
  * @date 01/04/2025
- * @description Arquivo de configuração da API do Marketplace de Hardware.
- * Este arquivo configura os middlewares, conecta ao banco de dados usando Prisma,
- * e define as rotas básicas da aplicação.
+ * @description Arquivo responsável pela configuração da aplicação Express.
  */
 
 import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
-import dotenv from 'dotenv';
-import { PrismaClient } from '@prisma/client';
 import router from './src/routes/Routes_Crud';
 
-/**
- * Importações:
- *
- * Express: Framework para criar a API e gerenciar rotas.
- * CORS: Middleware para permitir requisições de diferentes domínios.
- * Morgan: Middleware para logar requisições HTTP.
- * Dotenv: Carrega variáveis de ambiente do arquivo .env.
- * PrismaClient: Facilita a comunicação com o banco de dados.
- */
-
-// Carregando as variáveis de ambiente do arquivo .env
-dotenv.config();
-
 const app = express(); // Inicializa o aplicativo Express
-const prisma = new PrismaClient(); // Inicializa o PrismaClient para comunicação com o banco de dados
-
-// Usando a variável de ambiente PORT do .env, caso não esteja definida, usa a porta 3000
-const PORT = process.env.PORT || 3000;
-
-/**
- * Função para testar a conexão com o banco de dados usando Prisma.
- * @throws {Error} Se a conexão com o banco de dados falhar, o processo será encerrado.
- */
-async function testDatabaseConnection() {
-  try {
-    await prisma.$connect(); // Tenta se conectar ao banco de dados
-    console.log(
-      '✅ Conexão com o banco de dados estabelecida',
-    );
-  } catch (error) {
-    console.error(
-      '❌ Falha ao conectar ao banco de dados:',
-      error,
-    );
-    process.exit(1); // Encerra o processo se a conexão falhar
-  }
-}
 
 // Middleware global para permitir requisições de outros domínios
 app.use(cors());
@@ -84,13 +44,8 @@ app.get('/api/health', (req, res) => {
 app.get('/', (req, res) => {
   res.send('API está funcionando!');
 });
-// Conectar as rotas ao app
-app.use('/api', router); // Prefixando as rotas com '/api'
 
-// Iniciar o servidor na porta definida
-app.listen(PORT, async () => {
-  await testDatabaseConnection(); // Testa a conexão com o banco de dados ao iniciar o servidor
-  console.log(
-    `🚀 Servidor rodando em http://localhost:${PORT}`,
-  );
-});
+// Conectar as rotas da aplicação
+app.use('/api', router);
+
+export default app;
--- Back/src/service/Service_Pedido.ts
+++ Back/src/service/Service_Pedido.ts
@@ -1,6 +1,17 @@
 import { PrismaClient } from '@prisma/client';
 import ServiceCrud from './Service_Crud';
+import ServiceUser from './Service_User';
 
 class ServicePedido {
-  static async criar_pedido(data: any) {}
+  static async criar_pedido(data: any) {
+    const id_user =  await ServiceUser.getUserIdFromRequest(data.req);
+    if (!id_user) {
+      throw new Error('Usuário não autenticado.');
+    }
+    data.usuarios_id = id_user;
+    ServiceCrud.create('pedidos', data.pedido);
+    const id_pedido = data.pedido.id;
+    ServiceCrud.create('itens_pedidos', data.itens_pedidos);
+    return await ;
+  }
 }
